a7938c07996c2a79739d7685435fbf389f9def0f
wxyy <137875838+wxyydesu@users.noreply.github.com>
2025-03-16 21:59:13 +0700

    Update DISCORD RISK.bat

--- DISCORD RISK.bat
+++ DISCORD RISK.bat
@@ -1,5 +1,6 @@
 @echo off
-cd C:\Users\Lenovo\Documents\Ngoding\Virtualbox Risk\Virtual-Box-Discord-RPC-main
+:: jangan lupa Sesuaikan path VB MASTERNYA | RPC By @ljooker (*ON DISCORD*) | I forgot the link 
+cd C:\Virtualbox Risk\Virtual-Box-Discord-RPC-main
 echo Running Discord RPC
 echo Pls Wait....
 python "VBox Discord RPC.py"
